5c2e3c1737c667d9e2c388c296874f4bade83928
Daniel Hazelton <dshadowwolf@gmail.com>
2025-07-03 17:32:08 -0400

    add a test file to disassemble and a simple driver-wrapper

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "state.h"
+
+extern void disasm(char *filename);
+
+int main() {
+    // this is a quick and dirty test to see if this thing works
+    char *filename = "testfile.bin"; // replace with your test file
+    disasm(filename);
+    return 0;
+}
